13c81345649886f31e5f9cdc06a65ed61b2880b8
srisathyamamidala <77813332+srisathyamamidala@users.noreply.github.com>
2021-11-15 21:27:54 -0600

    backend written and changed the stylesheet

--- app.js
+++ app.js
@@ -1,66 +1,61 @@
-//getting all the required elements
-const inputBox = document.querySelector(".inputField input");
-const addBtn = document.querySelector(".inputField button");
-const habitList = document.querySelector(".habitList");
-const deleteAllBtn = document.querySelector(".footer button")
-
-inputBox.onkeyup = ()=>{
-let userData = inputBox.value; // getting user entered value
-if(userData.trim() != 0){// if user values are'nt only spaces
-    addBtn.classList.add("active"); // active the add button
-}
-else{
-    addBtn.classList.remove("active"); // unactive the add button
-
-}
-}
-showTasks();
-
-// if user click on the add button 
-addBtn.onClick = ()=>{
-    let userData = inputBox.value; // getting user entered value
-    let getLocalStorage = localStorage.getItem("New Habit");// getting localstorage
-    if(getLocalStorage == null)//if localstorage is null
-   
-{
-   listArr = []; // creating blank array
-}else{
-    listArr = JSON.parse(getLocalStorage);//transforming js string into a json object
- }
- listArr.push(userData);//pushing or adding user data
- localStorage.setItem("New Habit", JSON.stringify(listArr)); //transforming js object into a json string
- showTasks();//calling showtasks function
+document.querySelector('#push').onclick = function
+(){
+    if(document.querySelector('#newtask input').
+    value.length == 0){
+        alert("Please enter a Task")
+    }
+    else{
+        document.querySelector('#tasks').innerHTML
+        += `
+           <div class="task">
+              <span id="taskname">
+                  ${document.querySelector
+                  ('#newtask input').value}
+                  </span>
+                  
+                </div>
+            `;
+    }
 }
 
-//function to add the task list inside ul
-function showTasks(){
-    let getLocalStorage = localStorage.getItem("New Habit");//getting local storage
-    if(getLocalStorage == null){//if localstorage is null
-        listArr = []; // creating blank array
-      }else{     
-        listArr = JSON.parse(getLocalStorage);//transforming js string into a json object
-      } 
-      let newLiTag = '';
-      listArr.forEach((element, index) => {
-        newLiTag += `<li> ${element} <span onclick = "deleteTask(${index})";> <i class = "fas fa-trash"></i></span></li>`;
-    });
-        habitList.innerHTML = newLiTag; // adding new li tag inside ul tag
-        inputBox.value = "";//once task added leave the input field blank
+document.querySelector('#push').onclick = function
+(){
+    if(document.querySelector('#newtask input').
+    value.length == 0){
+        alert("Please enter a Task")
+
+    }
+    else{
+        document.querySelector('#tasks').innerHTML
+        += `
+            <div class="task">
+               <span id="taskname">
+                  ${document.querySelector
+                  ('#newtask input').value}
+                </span>
+                <button class="delete">
+                    <i class="far fa-trash-alt"></
+                    i>
+                </button>
+                
+            </div>
+        `;
+
+        var current_tasks = document.   
+        querySelectorAll(".delete");
+        for(var i=0; i<current_tasks.length; i++){
+            current_tasks[i].onclick =  function(){
+                this.parentNode.removeChild();
+            }
         }
-        //delete task function 
-        function deleteTask(index){
-        let getLocalStorage = localStorage.getItem("New Habit");
-        listArr = JSON.parse(getLocalStorage); 
-        listArr.splice(index,  1);//delete or remove the particular indexed li 
-        //after removing the li again update the local storage
-        localStorage.setItem("New Todo", JSON.stringify(listArr)); //transforming js object
-        
-         showTasks();// calling showtasks function
+        var tasks =  document.querySelectorAll(".task");
+        for(var i=0; i<tasks.length; i++){
+            tasks[i].onclick = function(){
+                this.classList.toggle('completed');
+
+            }
         }
-        // delete all tasks function
-        deleteAllBtn.onclick = ()=>{
-        listArr = [];//empty an array
-        // after delete all tasks the li again update the local storage
-        localStorage.setItem("New Habit", JSON.stringify(listArr));//transforming js object
-        showTasks();
-        }
+        document.querySelector("#newtask input").
+        value = "";
+    }
+}
