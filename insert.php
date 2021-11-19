9b407782a8f0a0e71e5efa544a6aedde027d5420
srisathyamamidala <77813332+srisathyamamidala@users.noreply.github.com>
2021-11-19 01:53:05 -0600

    trying to connect database

---
+++ insert.php
@@ -0,0 +1,27 @@
+<?php
+$host = "localhost";
+$user = "root";
+$password = "";
+$db = "demo";
+
+mysql_connect($host,$user,$password);
+mysql_select_db($db);
+
+if(isset($_POST['username'])){
+    $uname=$_POST['username'];
+    $password=$_POST['password'];
+
+    $sql="select * from loginform where user='".."'AND Pass='".."' limit 1";
+
+    $result=mysql_query($sql);
+
+    if(mysql_num_rows($result)==1){
+        echo " You have Successfully logged in";
+        exit();
+
+    }else{
+        echo " You have Entered Incorrect Password";
+        exit();
+    }
+    
+?>
