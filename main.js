3e72e54b122ce0379fc878bff649b4a4905e0e3d
srisathyamamidala <77813332+srisathyamamidala@users.noreply.github.com>
2021-11-28 20:27:04 -0600

    updated main and mainjs to connect to databse

--- main.js
+++ main.js
@@ -1,55 +1,62 @@
+
 var firebaseConfig = {
-    apiKey: "AIzaSyBKzv9PlQESIiA1eJoH9p4sLhkMBAdIeGE",
-    authDomain: "habit-reminder-f7fd7.firebaseapp.com",
-    databaseURL: "https://habit-reminder-f7fd7-default-rtdb.firebaseio.com",
-    projectId: "habit-reminder-f7fd7",
-    storageBucket: "habit-reminder-f7fd7.appspot.com",
-    messagingSenderId: "502767252044",
-    appId: "1:502767252044:web:715e5833dbe650839ffae9",
-    measurementId: "G-LX376GXF84"
-   };
-   
-     firebase.initializeApp(firebaseConfig);
-     firebase.analytics();
-   
-   
-   
-   var contactInfo = firebase.database().ref("users");
-   
-   
-   document.querySelector(".contact-form").addEventListener("submit", submitForm);
-   
-   
-   function submitForm(e) {
-     e.preventDefault();
-   
-     var name = document.querySelector(".name").value;
-     var email = document.querySelector(".email").value;
-     var message = document.querySelector(".message").value;
-     console.log(name, email, message);
-   
-     saveContactInfo(name, email, message);
-   
-     document.querySelector(".contact-form").reset();
-   
-   
-     document.getElementById('dis').style.visibility ='visible';
-   
-     setTimeout(function(){
-     document.getElementById('dis').style.visibility ='visible';
-     },3000);
-   }
-   
-   function saveContactInfo(name, email, message) {
-     //var newContactInfo = contactInfo.push();
-   email=email.replace(".", ",")
-     contactInfo.child(email).set({
-       name: name,
-       email: email,
-       message: message,
-     });
-   }
-   
-   
-   
-   
+  /* apiKey: "AIzaSyAnKu4gzCKQYYBbB8xDtxQ8GWJuAU8ckcM",
+      authDomain: "travelinn-8527c.firebaseapp.com",
+      databaseURL: "https://travelinn-8527c.firebaseio.com",
+      projectId: "travelinn-8527c",
+      storageBucket: "travelinn-8527c.appspot.com",
+      messagingSenderId: "806456585313",
+      appId: "1:806456585313:web:2aec374cc59b76f9458e20"*/
+    apiKey: "AIzaSyB0O_LnnmjsB4Ucni2a6Qw9ToxP4ikTFSA",
+    authDomain: "habitreminder-d891d.firebaseapp.com",
+    projectId: "habitreminder-d891d",
+    storageBucket: "habitreminder-d891d.appspot.com",
+    messagingSenderId: "1997366263",
+    appId: "1:1997366263:web:418c0a8b1b494ecb0704f2",
+    measurementId: "G-KTF0VDC0NV"
+  };
+  
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+  
+  
+  
+  var contactInfo = firebase.database().ref("users");
+  
+  
+  document.querySelector(".contact-form").addEventListener("submit", submitForm);
+  
+  
+  function submitForm(e) {
+    e.preventDefault();
+  
+    var name = document.querySelector(".name").value;
+    var email = document.querySelector(".email").value;
+    var message = document.querySelector(".message").value;
+    console.log(name, email, message);
+  
+    saveContactInfo(name, email, message);
+  
+    document.querySelector(".contact-form").reset();
+  
+  
+    document.getElementById('dis').style.visibility ='visible';
+  
+    setTimeout(function(){
+    document.getElementById('dis').style.visibility ='visible';
+    },3000);
+  }
+  
+  function saveContactInfo(name, email, message) {
+    //var newContactInfo = contactInfo.push();
+  email=email.replaceAll(".", ",")
+    contactInfo.child(email).set({
+      name: name,
+      email: email,
+      message: message,
+    });
+  }
+  
+  
+  
+  
--- main1.js
+++ main1.js
@@ -1,73 +1,72 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyBKzv9PlQESIiA1eJoH9p4sLhkMBAdIeGE",
-    authDomain: "habit-reminder-f7fd7.firebaseapp.com",
-    databaseURL: "https://habit-reminder-f7fd7-default-rtdb.firebaseio.com",
-    projectId: "habit-reminder-f7fd7",
-    storageBucket: "habit-reminder-f7fd7.appspot.com",
-    messagingSenderId: "502767252044",
-    appId: "1:502767252044:web:715e5833dbe650839ffae9",
-    measurementId: "G-LX376GXF84"
-   };
-   
-    const app = firebase.initializeApp(firebaseConfig);
-     firebase.analytics();
-   
-   
-   //const database = getDatabase(app);
-   
-   
-   var contactInfo = firebase.database().ref();
-   
-   document.querySelector("#login").addEventListener("click", submitLogin);
-   
-   function submitLogin(e) {
-   
-    contactInfo.on('value', snapshot => {    
-        dbRef = firebase.database().ref();
-       //alert(dbRef);
-       var email = document.querySelector("#email").value;
-       
-       email=email.replace(".", ",");
-   
-     dbRef1=dbRef.child("users").child(email).child("email");
-     dbRef2=dbRef.child("users").child(email).child("message");
-     //alert(dbRef1);
-     dbRef1.on('value', function(snapshot) {
-       //console.log(snapshot.val());
-       //string.localeCompare(String_2);
-   
-       if(snapshot.val()===null)
-           alert("invalid username");
-       else
-           checkPwd(dbRef2);
-           
-   
-       //snapshot.forEach(function(data) {
-         //  console.log(data.key);
-           //console.log(data.val());
-     //  });
-     
-     
-   });
-   });
-   
-         //console.log();
-   
-   }
-   function checkPwd(a){
-       
-       var pwd = document.querySelector("#pwd").value;
-       a.on('value', function(snapshot) {
-           alert(snapshot.val());
-       if(snapshot.val()===pwd)
-           alert("Login Successfull");
-       else
-           alert("Login failed");
-           
-           
-           
-           
-       });
-       
-       
-   }
+    apiKey: "AIzaSyB0O_LnnmjsB4Ucni2a6Qw9ToxP4ikTFSA",
+    authDomain: "habitreminder-d891d.firebaseapp.com",
+    projectId: "habitreminder-d891d",
+    storageBucket: "habitreminder-d891d.appspot.com",
+    messagingSenderId: "1997366263",
+    appId: "1:1997366263:web:418c0a8b1b494ecb0704f2",
+    measurementId: "G-KTF0VDC0NV"
+  };
+  
+   const app = firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+  
+  
+  //const database = getDatabase(app);
+  
+  
+  var contactInfo = firebase.database().ref();
+  
+  document.querySelector("#login").addEventListener("click", submitLogin);
+  
+  function submitLogin(e) {
+  
+   contactInfo.on('value', snapshot => {    
+       dbRef = firebase.database().ref();
+      //alert(dbRef);
+      var email = document.querySelector("#email").value;
+      
+      email=email.replaceAll(".", ",");
+  
+    dbRef1=dbRef.child("users").child(email).child("email");
+    dbRef2=dbRef.child("users").child(email).child("message");
+    //alert(dbRef1);
+    dbRef1.on('value', function(snapshot) {
+      //console.log(snapshot.val());
+      //string.localeCompare(String_2);
+  
+      if(snapshot.val()===null)
+          alert("invalid username");
+      else
+          checkPwd(dbRef2);
+          
+  
+      //snapshot.forEach(function(data) {
+        //  console.log(data.key);
+          //console.log(data.val());
+    //  });
+    
+    
+  });
+  });
+  
+        //console.log();
+  
+  }
+  function checkPwd(a){
+      
+      var pwd = document.querySelector("#pwd").value;
+      a.on('value', function(snapshot) {
+          alert(snapshot.val());
+      if(snapshot.val()===pwd)
+          alert("Login Successfull");
+      else
+          alert("Login failed");
+          
+          
+          
+          
+      });
+      
+      
+  }
